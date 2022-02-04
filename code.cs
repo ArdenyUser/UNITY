601cfb1cba29cbc1616db2d59200fe33d99f8a47
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2022-02-04 15:36:47 -0500

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System;
+using System.IO;
+
+// SAVE
+class Test
+{
+    public static void Main()
+    {
+        string path = @"c:\temp\MyTest.txt";
+        // This text is added only once to the file.
+        if (!File.Exists(path))
+        {
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine("Hello");
+            }	
+        }
+    }
+}
+
+// LOAD
+
+using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+ 
+public class LineReader : MonoBehaviour
+{
+    protected FileInfo theSourceFile = null;
+    protected StreamReader reader = null;
+    protected string text = " "; // assigned to allow first line to be read below
+ 
+    void Start () {
+        theSourceFile = new FileInfo ("Test.txt");
+        reader = theSourceFile.OpenText();
+    }
+   
+    void Update () {
+        if (text != null) {
+            text = reader.ReadLine();
+            //Console.WriteLine(text);
+            print (text);vg
+        }
+    }
+}
